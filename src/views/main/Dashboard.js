6d6051858053c52baa5f9a1d248e915c5fe1377a
eseku <joojoquartey11@gmail.com>
2021-09-22 13:43:09 +0000

    feat: views implementation

--- src/views/main/Dashboard.js
+++ src/views/main/Dashboard.js
@@ -1,11 +1,170 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  NavItem,
+  NavLink,
+  Nav,
+  Progress,
+  Table,
+  Container,
+  Row,
+  Col,
+  Spinner,
+} from "reactstrap";
+// import { chartOptions, parseOptions, chartExample1 } from "variables/charts.js";
 
 const Dashboard = () => {
-    return (
-        <div>
-            dash
-        </div>
-    )
-}
+  const [activeNav, setActiveNav] = useState(1);
+  const [chartExample1Data, setChartExample1Data] = useState("data1");
 
-export default Dashboard
+  //   if (window.Chart) {
+  //     parseOptions(Chart, chartOptions());
+  //   }
+
+  //   const toggleNavs = (e, index) => {
+  //     e.preventDefault();
+  //     setActiveNav(index);
+  //     setChartExample1Data("data" + index);
+  //   };
+  return (
+    <div>
+      {/* <Container className="mt--7" fluid>
+        <Row>
+          <Col className="mb-5 mb-xl-0" xl="8">
+            <Card className="bg-gradient-default shadow">
+              <CardHeader className="bg-transparent">
+                <Row className="align-items-center">
+                  <div className="col">
+                    <h6 className="text-uppercase text-light ls-1 mb-1">
+                      Overview
+                    </h6>
+                    <h2 className="text-white mb-0">Emergency</h2>
+                  </div>
+                  <div className="col">
+                    <Nav className="justify-content-end" pills>
+                      <NavItem>
+                        <NavLink
+                          className={classnames("py-2 px-3", {
+                            active: activeNav === 1,
+                          })}
+                          // href="#pablo"
+                          onClick={e => toggleNavs(e, 1)}
+                        >
+                          <span className="d-none d-md-block">Month</span>
+                          <span className="d-md-none">M</span>
+                        </NavLink>
+                      </NavItem>
+                      <NavItem>
+                        <NavLink
+                          className={classnames("py-2 px-3", {
+                            active: activeNav === 2,
+                          })}
+                          data-toggle="tab"
+                          href="#pablo"
+                          onClick={e => toggleNavs(e, 2)}
+                        >
+                          <span className="d-none d-md-block">Week</span>
+                          <span className="d-md-none">W</span>
+                        </NavLink>
+                      </NavItem>
+                    </Nav>
+                  </div>
+                </Row>
+              </CardHeader>
+              <CardBody>
+                <div className="chart">
+                  <Line
+                    data={chartExample1[chartExample1Data]}
+                    options={chartExample1.options}
+                    getDatasetAtEvent={e => console.log(e)}
+                  />
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col className xl="4">
+            <Card className="shadow">
+              <CardHeader className="border-0">
+                <Row className="align-items-center">
+                  <div className="col">
+                    <h3 className="mb-0">Emergency Units</h3>
+                  </div>
+                  <div className="col text-right">
+                    <Button
+                      color="primary"
+                      href="#pablo"
+                      onClick={e => e.preventDefault()}
+                      size="sm"
+                    >
+                      See all
+                    </Button>
+                  </div>
+                </Row>
+              </CardHeader>
+              <Table className="align-items-center table-flush" responsive>
+                <thead className="thead-light">
+                  <tr>
+                    <th scope="col">Unit</th>
+                    <th scope="col">Frequency</th>
+                    <th scope="col" />
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">Police</th>
+                    <td>20</td>
+                    <td>
+                      <div className="d-flex align-items-center">
+                        <span className="mr-2">70%</span>
+                        <div>
+                          <Progress
+                            max="100"
+                            value="20"
+                            barClassName="bg-gradient-danger"
+                          />
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Ambulance</th>
+                    <td>50</td>
+                    <td>
+                      <div className="d-flex align-items-center">
+                        <span className="mr-2">40%</span>
+                        <div>
+                          <Progress
+                            max="100"
+                            value="40"
+                            barClassName="bg-gradient-success"
+                          />
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Fire Service</th>
+                    <td>70</td>
+                    <td>
+                      <div className="d-flex align-items-center">
+                        <span className="mr-2">70%</span>
+                        <div>
+                          <Progress max="100" value="80" />
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Card>
+          </Col>
+        </Row>
+      </Container> */}
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/views/main/Report.js
+++ src/views/main/Report.js
@@ -1,10 +1,189 @@
 import React from "react";
-import { Footer } from "../../components/gen/";
+import { Footer } from "../../components/gen";
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardFooter,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+  Table,
+  Container,
+  Row,
+} from "reactstrap";
 
 const Notifications = () => {
   return (
     <div>
-      not
+      <Container className="mt--10" fluid>
+        <Row>
+          <div className="col">
+            <Card className="shadow">
+              <CardHeader className="border-0">
+                <h3 className="mb-0">Emergency Reports</h3>
+              </CardHeader>
+              <Table className="align-items-center table-flush" responsive>
+                <thead className="thead-light">
+                  <tr>
+                    <th scope="col">Post ID</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Severity</th>
+                    <th scope="col">Location</th>
+                    <th scope="col">View Details</th>
+                    <th scope="col">Dispatch</th>
+                    <th />
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* {report.map((item, index) => {
+                    getURL(item.postId);
+                    return (
+                      <tr key={index}>
+                        <td>{item.posterUserID}</td>
+                        <td>{item.title}</td>
+                        <td>{item.postSeverity === true ? "High" : "Low"}</td>
+                        <td>{item.town === "" ? "Legon" : item.town}</td>
+                        <td>
+                          <Button
+                            color="info"
+                            onClick={() =>
+                              openReportModal({
+                                postId: item.postId,
+                                lat: item.postRegion.latitude,
+                                long: item.postRegion.longitude,
+                                date: item.date,
+                                description: item.description,
+                                time: item.time,
+                                type: item.type,
+                                verified: item.verified.verified,
+                                numComments: item.numComments,
+                                // postImage: ,
+                                postImg: img,
+                              })
+                            }
+                            size="sm"
+                          >
+                            View Details
+                          </Button>
+                        </td>
+                        <td>
+                          {status === false ? (
+                            <Button
+                              color="success"
+                              onClick={() =>
+                                dispatchFunc({
+                                  town: item.town,
+                                  type: item.type,
+                                })
+                              }
+                              size="sm"
+                            >
+                              Dispatch
+                            </Button>
+                          ) : (
+                            <Button
+                              color="danger"
+                              onClick={e => e.preventDefault()}
+                              size="sm"
+                            >
+                              Proccessing
+                            </Button>
+                          )}
+                        </td>
+                        <td className="text-right">
+                          <UncontrolledDropdown>
+                            <DropdownToggle
+                              className="btn-icon-only text-light"
+                              role="button"
+                              size="sm"
+                              color=""
+                              onClick={e => e.preventDefault()}
+                            >
+                              <i className="fas fa-ellipsis-v" />
+                            </DropdownToggle>
+                            <DropdownMenu className="dropdown-menu-arrow" right>
+                              <DropdownItem
+                                href="/admin/report"
+                                onClick={() => removePosts(item.postId)}
+                              >
+                                Delete
+                              </DropdownItem>
+                              <DropdownItem onClick={e => e.preventDefault()}>
+                                Forward
+                              </DropdownItem>
+                              <DropdownItem onClick={e => e.preventDefault()}>
+                                Reply
+                              </DropdownItem>
+                            </DropdownMenu>
+                          </UncontrolledDropdown>
+                        </td>
+                      </tr>
+                    );
+                  })} */}
+                </tbody>
+              </Table>
+              <CardFooter className="py-4">
+                <nav aria-label="...">
+                  <Pagination
+                    className="pagination justify-content-end mb-0"
+                    listClassName="justify-content-end mb-0"
+                  >
+                    <PaginationItem className="disabled">
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={e => e.preventDefault()}
+                        tabIndex="-1"
+                      >
+                        <i className="fas fa-angle-left" />
+                        <span className="sr-only">Previous</span>
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem className="active">
+                      <PaginationLink
+                        className="bg-info"
+                        href="#pablo"
+                        onClick={e => e.preventDefault()}
+                      >
+                        1
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={e => e.preventDefault()}
+                      >
+                        2 <span className="sr-only">(current)</span>
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={e => e.preventDefault()}
+                      >
+                        3
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={e => e.preventDefault()}
+                      >
+                        <i className="fas fa-angle-right" />
+                        <span className="sr-only">Next</span>
+                      </PaginationLink>
+                    </PaginationItem>
+                  </Pagination>
+                </nav>
+              </CardFooter>
+            </Card>
+          </div>
+        </Row>
+      </Container>
       <Footer />
     </div>
   );
